830fb300709c145760b20a17b09a946a56dbf9c9
Neil Buesing <neil@kineticedge.io>
2023-02-26 14:47:00 -0600

    cleanup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -8,11 +8,6 @@ if ! [ -x "$(command -v docker)" ]; then
     exit 1
 fi
 
-#if ! [ -x "$(command -v docker-compose)" ]; then
-#    echo "docker-compose is not installed." >&2
-#    exit 1
-#fi
-
 docker info > /dev/null 2>&1
 if [ $? -ne 0 ]; then
   echo "docker server is not running." >&2
@@ -24,17 +19,15 @@ fi
 # kafka-streams-101 -> ks101
 #
 NETWORK=$(docker network inspect -f '{{.Name}}' ks101 2>/dev/null)
-if [ "$NETWORK" != "ksd" ]; then
+if [ "$NETWORK" != "ks101" ]; then
   (docker network create ks101 >/dev/null)
 fi
 
 (cd cluster_zk; docker-compose up -d --wait)
 #(cd cluster; docker-compose up -d --wait)
 
-#./gradlew build
+./gradlew build
 
-#(cd builder; ../gradlew run)
-#(cd monitoring; docker-compose up -d)
-#(cd applications; docker-compose up -d)
+(cd applications; docker-compose up -d)
+(cd monitoring; docker-compose up -d)
 
-#(cd publisher; ../gradlew run --args="--max-sku 100")
